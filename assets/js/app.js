66443c066444567f332980382115525c1481ef9d
Jibon360 <mjjibon115@gmail.com>
2023-01-05 22:22:16 +0600

    about and experience/progressbar section done

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,4 +25,42 @@ $(function (params) {
     loop: true,
     loopCount: Infinity,
   });
+
+  // ! banner water ripple effect animation
+
+  $(".banner-fluid").ripples({
+    resolution: 512,
+    dropRadius: 20,
+    perturbance: 0.04,
+  });
+  // ! about typing animation
+  var typed = new Typed(".runtextabout", {
+    strings: ["FULL STACK WEB DEVELOPER", "GRAPHICS DESINGER", "UI/UI EXPERT"],
+    smartBackspace: true,
+    typeSpeed: 100,
+    startDelay: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+    loopCount: Infinity,
+  });
+
+
+
+
+  // ! animated skillbar
+  var p = document.querySelectorAll(".progress-bar");
+  p[0].setAttribute("style", "width:98%;transition: 1.5s all");
+  p[1].setAttribute("style", "width:90%;transition: 1.7s all");
+  p[2].setAttribute("style", "width:75%;transition: 2.5s all");
+  p[3].setAttribute("style", "width:88%;transition: 2.7s all");
+  p[4].setAttribute("style", "width:85%;transition: 3s all");
+  p[5].setAttribute("style", "width:86%;transition: 3.5s all");
+
+
+
+
+
+
+
 });
